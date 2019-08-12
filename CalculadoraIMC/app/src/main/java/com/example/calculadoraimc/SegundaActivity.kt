7a25e036d67a4f468c5a205be349bdde71cbd1e3
Samuel <samuel1797@gmail.com>
2019-08-12 12:33:51 -0300

    Adicionando segunda atividade

---
+++ CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/SegundaActivity.kt
@@ -0,0 +1,15 @@
+package com.example.calculadoraimc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+
+//        val pesoOuAltura = intent
+
+    }
+}
